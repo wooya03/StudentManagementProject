d639f7824a020271598d264f43c6f5cc9a8cdcd4
Wooya <wooya_0@naver.com>
2024-04-22 20:40:54 +0900

    process

--- src/Process.java
+++ src/Process.java
@@ -1,6 +1,5 @@
 import management.StudentManagement;
 import management.StudentManagementSystem;
-
 public class Process {
     public static void main(String[] args) {
         StudentManagement studentManagement = new StudentManagementSystem();
